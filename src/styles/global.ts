1b7fa9d099e7e0ecfe8f68e65b5f6257d16be1df
Washington Campos <washingtoncampos9@gmail.com>
2022-02-15 15:36:55 -0300

    chore: add default font-family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,6 @@ export default createGlobalStyle`
 	}
 
 	body {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
 `;
