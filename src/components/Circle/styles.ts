5384929fb6d09241e7109137006aa22ea8b27fd6
Washington Campos <washingtoncampos9@gmail.com>
2022-02-16 15:51:57 -0300

    chore: add styles to Circle

--- src/components/Circle/styles.ts
+++ src/components/Circle/styles.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import styled, { css } from "styled-components";
+import media from "styled-media-query";
 
 import { CircleProps } from ".";
 
@@ -7,6 +8,21 @@ type WrapperProps = Pick<CircleProps, "bgColor">;
 
 export const Wrapper = styled.div<WrapperProps>`
 	${({ theme, bgColor }) => css`
+		width: 7.5rem;
+		height: 7.5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 50%;
+		font-size: ${theme.font.size.medium};
+		font-weight: ${theme.font.weight.bold};
+
+		${media.greaterThan("medium")`
+			width: 11rem;
+			height: 11rem;
+			font-size: ${theme.font.size.xlarge};
+		`}
+
 		background-color: ${theme.colors[bgColor!]};
 	`}
 `;
