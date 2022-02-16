7c0065ce19648230779e6d9c4cde1a5b85a9fc27
Washington Campos <washingtoncampos9@gmail.com>
2022-02-16 15:09:27 -0300

    test: ensure Heading renders with correct sizes

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -9,4 +9,16 @@ describe("<Heading />", () => {
 
 		expect(screen.getByRole("heading", { name: /my text/i })).toBeInTheDocument();
 	});
+
+	it("Should render Heading with medium size by default", () => {
+		renderWithTheme(<Heading>My text</Heading>);
+
+		expect(screen.getByRole("heading", { name: /my text/i })).toHaveStyle({ fontSize: "2.8rem" });
+	});
+
+	it("Should render Heading with small size when prop is passed", () => {
+		renderWithTheme(<Heading size="small">My text</Heading>);
+
+		expect(screen.getByRole("heading", { name: /my text/i })).toHaveStyle({ fontSize: "2.4rem" });
+	});
 });
