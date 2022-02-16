6e91278831b773caec736cca45ee9d036c1810fd
Washington Campos <washingtoncampos9@gmail.com>
2022-02-16 15:15:45 -0300

    feat: add color prop to Heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,8 +3,13 @@ import * as S from "./styles";
 export type HeadingProps = {
 	children: React.ReactNode;
 	size?: "small" | "medium";
+	color?: "black" | "white";
 };
 
-export const Heading = ({ children, size = "medium" }: HeadingProps) => {
-	return <S.Wrapper size={size}>{children}</S.Wrapper>;
+export const Heading = ({ children, size = "medium", color = "black" }: HeadingProps) => {
+	return (
+		<S.Wrapper size={size} color={color}>
+			{children}
+		</S.Wrapper>
+	);
 };
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -2,7 +2,7 @@ import styled, { css, DefaultTheme } from "styled-components";
 
 import { HeadingProps } from ".";
 
-type WrapperProps = Pick<HeadingProps, "size">;
+type WrapperProps = Pick<HeadingProps, "size" | "color">;
 
 const wrapperModifier = {
 	small: (theme: DefaultTheme) => css`
@@ -11,10 +11,18 @@ const wrapperModifier = {
 	medium: (theme: DefaultTheme) => css`
 		font-size: ${theme.font.size.xlarge};
 	`,
+
+	black: (theme: DefaultTheme) => css`
+		color: ${theme.colors.black};
+	`,
+	white: (theme: DefaultTheme) => css`
+		color: ${theme.colors.white};
+	`,
 };
 
 export const Wrapper = styled.h2<WrapperProps>`
-	${({ theme, size }) => css`
+	${({ theme, size, color }) => css`
 		${!!size && wrapperModifier[size](theme)};
+		${!!color && wrapperModifier[color](theme)};
 	`}
 `;
