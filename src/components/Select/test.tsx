58e1348eef9d387a556dec6763fa7a4860b2ff76
Washington Campos <washingtoncampos9@gmail.com>
2022-02-17 13:27:59 -0300

    test: ensure Select renders default value correctly

--- src/components/Select/test.tsx
+++ src/components/Select/test.tsx
@@ -7,7 +7,7 @@ import { Select } from ".";
 
 describe("<Select />", () => {
 	it("Should render Select with correct options", async () => {
-		renderWithTheme(<Select onChange={() => ({})} />);
+		renderWithTheme(<Select />);
 
 		expect(screen.getByRole("combobox")).toBeInTheDocument();
 		expect(screen.getByRole("option", { name: /Escolha uma loteria/i })).toBeInTheDocument();
@@ -20,7 +20,7 @@ describe("<Select />", () => {
 	});
 
 	it("Should change select options", async () => {
-		renderWithTheme(<Select onChange={() => ({})} />);
+		renderWithTheme(<Select />);
 
 		const defaultOption = screen.getByRole("option", {
 			name: "Escolha uma loteria",
@@ -34,6 +34,16 @@ describe("<Select />", () => {
 		expect(defaultOption.selected).toBe(true);
 
 		userEvent.selectOptions(select, megaSenaOption);
+
+		expect(megaSenaOption.selected).toBe(true);
+	});
+
+	it("Should render default value correctly", async () => {
+		renderWithTheme(<Select defaultValue={0} />);
+
+		const megaSenaOption = (await screen.findByRole("option", {
+			name: /mega-sena/i,
+		})) as HTMLOptionElement;
 
 		expect(megaSenaOption.selected).toBe(true);
 	});
