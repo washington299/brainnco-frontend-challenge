b9413e3c96f3653bc6acb4edf3eb70f72b91d644
Washington Campos <washingtoncampos9@gmail.com>
2022-02-17 01:13:55 -0300

    test: ensure Select changes options correctly

--- src/components/Select/test.tsx
+++ src/components/Select/test.tsx
@@ -1,4 +1,6 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import { renderWithTheme } from "utils/tests/helpers";
 
 import { Select } from ".";
@@ -15,5 +17,23 @@ describe("<Select />", () => {
 		expect(await screen.findByRole("option", { name: /lotomania/i })).toBeInTheDocument();
 		expect(await screen.findByRole("option", { name: /timemania/i })).toBeInTheDocument();
 		expect(await screen.findByRole("option", { name: /dia de sorte/i })).toBeInTheDocument();
+	});
+
+	it("Should change select options", async () => {
+		renderWithTheme(<Select />);
+
+		await screen.findByRole("option", { name: /mega-sena/i });
+
+		const select = screen.getByRole("combobox");
+
+		expect(
+			(screen.getByRole("option", { name: "Escolha uma loteria" }) as HTMLOptionElement).selected,
+		).toBe(true);
+
+		userEvent.selectOptions(select, screen.getByRole("option", { name: "mega-sena" }));
+
+		expect((screen.getByRole("option", { name: "mega-sena" }) as HTMLOptionElement).selected).toBe(
+			true,
+		);
 	});
 });
