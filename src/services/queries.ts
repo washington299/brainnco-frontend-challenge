4b7f23202f7271ede07f63c37645a4ed6dc289cb
Washington Campos <washingtoncampos9@gmail.com>
2022-02-20 15:57:06 -0300

    feat: create getLotteriesContests and getContest query methods

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -2,4 +2,6 @@ import { api } from "./config";
 
 export const QUERY = {
 	getLotteries: () => api.get("/loterias"),
+	getLotteriesContests: () => api.get("/loterias-concursos"),
+	getContest: (param: string | string[]) => api.get(`/concursos/${param}`),
 };
